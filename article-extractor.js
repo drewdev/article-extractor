78ebc71b5c38e7caed55f6430325c90b4005a0ea
Drew Bisset <andrew.bisset@gmail.com>
2017-05-23 18:39:19 -0400

    wip

--- article-extractor.js
+++ article-extractor.js
@@ -2,6 +2,8 @@ var matchedElements,
   targetedContent,
   scrubbedContent,
   extractedArticle,
+  elementsToSelect = ['article','section'],
+  tagsToScrub = ['style','noscript','script','iframe','footer'],
   sortByInnerHTML = function(a,b) { return (a.innerHTML.length < b.innerHTML.length) ? 1 : -1; };
 
 function scrub(tagName, raw) {
@@ -17,19 +19,22 @@ function scrub(tagName, raw) {
   }
 }
 
-matchedElements = ['article','section'].reduce(function(accumulator, tag) {
+matchedElements = elementsToSelect.reduce(function(accumulator, tag) {
   return accumulator.concat(Array.from(document.getElementsByTagName(tag)));
 }, []);
 
-targetedContent = matchedElements.sort(sortByInnerHTML)[0];
+if (matchedElements.length > 0) {
+  matchedElements.forEach(function(element) {
+    scrubbedContent = tagsToScrub.reduce(function(accumulator, tagToScrub) {
+      return accumulator = scrub(tagToScrub, accumulator);
+    }, element.innerHTML);
+  });
 
-scrubbedContent = ['iframe','footer'].reduce(function(accumulator, tagToScrub) {
-  return accumulator = scrub(tagToScrub, accumulator);
-}, targetedContent.innerHTML);
+  targetedContent = matchedElements.sort(sortByInnerHTML)[0];
 
+  extractedArticle = '<' + targetedContent.tagName + '>' +
+    scrubbedContent +
+    '</' + targetedContent.tagName + '>';
 
-extractedArticle = '<' + targetedContent.tagName + '>' +
-  scrubbedContent +
-  '</' + targetedContent.tagName + '>';
-
-document.getElementsByTagName('body')[0].innerHTML = extractedArticle;
+  document.getElementsByTagName('body')[0].innerHTML = extractedArticle;
+}
